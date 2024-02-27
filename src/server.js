14ab4c2984dd5b856c21e97edc0abe0c7c56af61
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-02-27 11:52:51 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -6,14 +6,24 @@ import http from "node:http";
 // PATCH => atualizar uma informação especifica de um recurso no back-end
 // DELETE => deletar um recurso no back-end
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuario");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "Jonh Doe",
+      email: "jonh@gmail.com",
+    });
+
     return res.end("Criação de usuario");
   }
 
