ae53cb39a639dc1f0fc2d428505cfd81d0a71e0d
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-02-27 11:28:24 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 import http from "node:http";
 
+// GET => buscar um recurso no back-end
+// POST => cria um recurso no back-end
+// PUT => atualizar um recurso no back-end
+// PATCH => atualizar uma informação especifica de um recurso no back-end
+// DELETE => deletar um recurso no back-end
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuario");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuario");
+  }
+
   return res.end("Hello World");
 });
 
