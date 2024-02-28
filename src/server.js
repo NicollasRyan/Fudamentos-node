2be1a6d3e8619cbead7c4494801fdeb775f5e0b2
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-02-28 09:39:36 -0300

    Criando banco de dados JSON

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Databese } from "./database.js";
 
 // GET => buscar um recurso no back-end
 // POST => cria um recurso no back-end
@@ -7,7 +8,7 @@ import { json } from "./middlewares/json.js";
 // PATCH => atualizar uma informação especifica de um recurso no back-end
 // DELETE => deletar um recurso no back-end
 
-const users = [];
+const database = new Databese();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -15,18 +16,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
     return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
 
+    database.insert("users", user);
     return res.writeHead(201).end();
   }
 
